0394ec4ddb289c4304c022b220040840a58bc067
adriano da rocha nunes <adrnrocha86@gmail.com>
2025-06-26 23:11:11 -0300

    atualização da etiqueta

--- ponto/utils_etiqueta.py
+++ ponto/utils_etiqueta.py
@@ -17,23 +17,23 @@ def gerar_etiqueta(nome_colaborador, nome_lider, dados_qr):  # nome_lider ainda 
     etiqueta_draw = ImageDraw.Draw(etiqueta)
 
     try:
-        fonte = ImageFont.truetype("arial.ttf", 30)  
+        fonte = ImageFont.truetype("arial.ttf", 17)  
     except IOError:
         fonte = ImageFont.load_default()
 
-    texto_colaborador = f"Colaborador: {nome_colaborador}"
+    texto_colaborador = f"{nome_colaborador}"
     bbox_colaborador = fonte.getbbox(texto_colaborador)
     largura_colaborador = bbox_colaborador[2] - bbox_colaborador[0]
 
     centro = etiqueta_largura // 2
     x_colaborador = centro - (largura_colaborador // 2)
 
-    etiqueta_draw.text((x_colaborador, 12), texto_colaborador, font=fonte, fill='black')
+    etiqueta_draw.text((x_colaborador, 10), texto_colaborador, font=fonte, fill='black')
 
     qr = qrcode.make(dados_qr)
     qr = qr.resize((120, 120))
     x_qr = centro - (qr.width // 2)
-    etiqueta.paste(qr, (x_qr, 60))  # ajustado um pouco para a nova altura da fonte
+    etiqueta.paste(qr, (x_qr, 45))  # ajustado um pouco para a nova altura da fonte
 
     pos_x = (folha_largura - etiqueta_largura) // 2
     pos_y = margem_superior
